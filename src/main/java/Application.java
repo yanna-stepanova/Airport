9bcb2487ce6335e65530d10ce611d6b6dc5615ab
Yana Stepanova <yanna.stepanova@mail.com>
2020-04-21 21:08:35 +0200

    fill Flight+FlightsInfo+FlightStore

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,12 @@
+import model.Flight;
+import model.FlightsInfo;
+import store.FlightStore;
+
 public class Application {
     public static void main(String[] args) {
         //можно здесь писать код, чтобы проверить, что все ваши сервисы работают правильно
+
+
+
     }
 }
--- src/main/java/model/FlightsInfo.java
+++ src/main/java/model/FlightsInfo.java
@@ -1,16 +1,18 @@
 package model;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class FlightsInfo {
+    private Set<Flight> flights = new HashSet<>();  // рейсы для которых будет отображаться информация
 
-    /**
-     * Конструктор принимает список рейсов
-     *
-     * @param flights - рейсы для которых будет отображаться информация
-     */
     public FlightsInfo(Set<Flight> flights) {
-
+        Iterator<Flight> iteratorFlights = flights.iterator();
+        while (iteratorFlights.hasNext()) {
+            this.flights.add(new Flight(iteratorFlights.next().getFlightNo(), iteratorFlights.next().getDeparture(),
+                    iteratorFlights.next().getDestination()));
+        }
     }
 
     /**
@@ -27,6 +29,16 @@ public class FlightsInfo {
      */
     @Override
     public String toString() {
-        return null;
+        do {
+            return String.format(".____________________.____________________.____________________.\n" +
+                            "| Flight #           | Departure          | Destination        |\n" +
+                            "|--------------------|--------------------|--------------------|\n" +
+                            "|%1$-20d|%2$-20s|%3$-20s|\n" +
+                            "|____________________|____________________|____________________|",
+                    flights.iterator().next().getFlightNo(), flights.iterator().next().getDeparture(),
+                    flights.iterator().next().getDestination());
+        }
+        while (flights.iterator().hasNext());
     }
+
 }
