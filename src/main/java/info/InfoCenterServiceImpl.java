c8737e3b5b82e0c79a29379fc1a4c8ae8f6f760b
Yana Stepanova <yanna.stepanova@mail.com>
2020-04-22 14:47:52 +0200

    first working version of airport

--- src/main/java/info/InfoCenterServiceImpl.java
+++ src/main/java/info/InfoCenterServiceImpl.java
@@ -2,20 +2,30 @@ package info;
 
 import model.FlightsInfo;
 import terminal.TerminalService;
+import terminal.TerminalServiceImpl;
 
 public class InfoCenterServiceImpl implements InfoCenterService {
+    private FlightsInfo flightsInfo;
+    private TerminalService terminal = new TerminalServiceImpl();
 
     public InfoCenterServiceImpl(TerminalService terminalService) {
-
+        this.terminal = terminalService;
+        this.terminal.addAllFlights(terminalService.getAllFlights());
+        flightsInfo = new FlightsInfo(terminal.getAllFlights());
     }
 
     @Override
     public FlightsInfo getAllFlights() {
-        return null;
+        return flightsInfo;
     }
 
     @Override
     public FlightsInfo getFlightsFrom(String departure) {
-        return null;
+        FlightsInfo flightsFrom = null;
+        if(flightsInfo!=null){
+            flightsFrom = new FlightsInfo(terminal.getFlightByDeparture(departure));
+
+        }
+        return flightsFrom;
     }
 }
